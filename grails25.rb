5422feb5b1f89616f07e30f62c03ea7168d634af
David Sawyer <davidsawyer10@gmail.com>
2015-12-02 17:10:01 -0500

    Add Grails 2.5.0

---
+++ grails25.rb
@@ -0,0 +1,27 @@
+class Grails25 < Formula
+  desc "Web application framework for the Groovy language"
+  homepage "http://grails.org"
+  url "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.5.0.zip"
+  sha256 "b65a3556bddbbc5fcea8195ad4e40793415df86518980fb5cb2f53c8576dd7d9"
+
+  bottle :unneeded
+
+  depends_on :java
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install %w[LICENSE README]
+    libexec.install Dir["*"]
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |f|
+      next unless File.extname(f).empty?
+      ln_s f, bin+File.basename(f)
+    end
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    assert_match "Grails version: #{version}",
+      shell_output("#{bin}/grails --version")
+  end
+end
